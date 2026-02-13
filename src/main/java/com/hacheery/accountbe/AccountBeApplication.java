29f8a67ff0a9fac6a490096b843cc380a8db6f2d
vanhach97 <vanhach97@gmail.com>
2026-02-13 22:23:13 +0700

    init project

--- src/main/java/com/hacheery/accountbe/AccountBeApplication.java
+++ src/main/java/com/hacheery/accountbe/AccountBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccountBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBeApplication.class, args);
+    }
 
 }
