b1c27ca7c65c7f9a43c63ef5af2508b168a5eb57
vanhach97 <vanhach97@gmail.com>
2026-02-13 22:23:03 +0700

    init project

---
+++ src/main/java/com/hacheery/accountbe/AccountBeApplication.java
@@ -0,0 +1,13 @@
+package com.hacheery.accountbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacheery/accountbe/AccountBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacheery.accountbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
